fd99012dbea548a59e64c10b097a235649ec218c
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-22 21:56:51 -0500

    Automate Sysmon Installation
    Requires further testing and we may want to note use certutil depending on the sitatuin

--- Windows/DeploySysmon.ps1
+++ Windows/DeploySysmon.ps1
@@ -1,1 +1,11 @@
+# Download Sysinternals Suite if not already installed
+certutil.exe -urlcache -f https://download.sysinternals.com/files/SysinternalsSuite.zip C:\Users\Administrators\Documents\SysinternalsSuite.zip
 
+# Download Sysmon Modular Configuration
+certutil.exe -urlcache -f https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml C:\Users\Administrators\Documents\sysmonconfig.xml
+
+# Install Sysmon
+C:\Users\Administrator\Documents\SysinternalsSuite\sysmon64 -i sysmonconfig.xml
+
+# Install the Manifest
+C:\Users\Administrator\Documents\SysinternalsSuite\sysmon64 -s
